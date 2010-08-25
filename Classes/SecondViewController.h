fbcc83b323cb3fde753a7e6a1f0f40670199f352
WrightsCS <aaron@wrightscs.com>
2010-08-24 22:09:57 -0600

    first commit

---
+++ Classes/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UITableViewController {
+
+}
+
+@end
---
+++ Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ___PROJECTNAMEASIDENTIFIER___AppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
